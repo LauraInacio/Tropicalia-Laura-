2b40eccad586d17ef7661dd0850cc0449bc813e6
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2025-04-01 08:34:30 -0300

    01/04

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-Document. addEventListener('DDMContentLoaded' , function (){
+document.addEventListener('DDMContentLoaded' , function (){
     const aumentaFonteBotao = documente.getElementeById ('aumentar-fonte')
 
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventeListener('clik' , function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.font.Size = '${tamanhoAtualFonte}rem';
+        document.body.style.font.Size = `${tamanhoAtualFonte}rem`;
     })
 })
